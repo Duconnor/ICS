d1b22e3d751e6d53f8249090c6e522aec39a61ad
monitor <tracer@njuics.org>
2018-09-09 20:58:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -264,7 +264,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
 	if (src > data_size)
 		cpu.eflags.CF = 0;
 	else
-		cpu.eflags.CF = dest >> (data_size - src);
+		cpu.eflags.CF = (dest >> (data_size - src)) & 1;
 	dest = dest << src;
 	dest &= mask; // cut out the higher bits
 	dest |= box; // put the saved bits back
