c1d7e23d4005e6e480c440b9258da406eed50cc2
tracer-pa2018-fall <tracer@njuics.org>
2018-09-12 20:40:24 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:40:24 up 18 min,  2 users,  load average: 0.01, 0.03, 0.00 19fe7d3bbcf6608a25d8ddb72602c1e06d5c05

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -229,7 +229,7 @@ uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size) {
 		dest &= mask_two;
 	}
 	assert(src != 0); // when src is zero, exit the program
-	return ((dest / src) & create_mask(data_size));
+	return ((uint32_t)(dest / src) & create_mask(data_size));
 #endif
 }
 
@@ -247,9 +247,18 @@ uint32_t alu_mod(uint64_t src, uint64_t dest) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mod(src, dest);
 #else
+	/*
 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	assert(0);
 	return 0;
+	*/
+	if (data_size < 32) {
+		uint32_t mask = create_mask(data_size), mask_two = create_mask(data_size * 2);
+		src &= mask;
+		dest &= mask_two;
+	}
+	assert(src != 0); // when src is zero, exit the program
+	return ((uint32_t)(dest % src) & create_mask(data_size));
 #endif
 }
 
