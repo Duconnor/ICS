3ab90b06a891139d77a75e70da94360539eb1540
monitor <tracer@njuics.org>
2018-09-14 19:09:01 +0800

    monitor test

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -18,10 +18,6 @@ void fpu_test_add() {
 			// printf(" == %d ==\n", count++);
 			a.fval = input[i];
 			b.fval = input[j];
-			if (a.val == 1 && b.val == 0xbf000000)
-			{
-				printf("catch!\n");
-			}
 			res.fval = a.fval + b.fval;
 			res_fpu.val = internal_float_add(b.val, a.val);
 			// comment here!!!
