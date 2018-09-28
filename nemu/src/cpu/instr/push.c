5b6459dad24f83770f7240ec099b02e9759cd0ce
tracer-pa2018-fall <tracer@njuics.org>
2018-09-28 22:19:14 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:19:14 up  2:43,  6 users,  load average: 0.00, 0.00, 0.00 3d1b393d286ffe60d75761c556c4378ec5105de5

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,8 +2,8 @@
 
 static void instr_execute_1op() {
 	operand_read(&opr_src);
-	CPU_STATE.esp -= (data_size / 8);
-	*CPU_STATE.esp = opr_src.val;
+	cpu.esp -= (data_size / 8);
+	*(cpu.esp) = opr_src.val;
 }
 
 make_instr_impl_1op(push, r, v);
