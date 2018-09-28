f91e7a9bc7c23552b44a76367847800e02d80863
tracer-pa2018-fall <tracer@njuics.org>
2018-09-28 20:39:46 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:39:46 up  1:34,  5 users,  load average: 0.01, 0.00, 0.00 a6f10d4d53956094f72a8d4b77910925d3b183

---
+++ nemu/src/cpu/instr/add.c
@@ -0,0 +1,10 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest = alu_add(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(add, i, rm, bv);
