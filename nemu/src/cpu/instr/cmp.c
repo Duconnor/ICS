bc9778141e5f983598c1b1499b699aea682a138e
tracer-pa2018-fall <tracer@njuics.org>
2018-09-28 21:50:31 +0800

    >  clean 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:50:31 up  2:14,  5 users,  load average: 0.00, 0.00, 0.00 30b2de5dbf9e5a96a027c85ff300f1e472c96eba

---
+++ nemu/src/cpu/instr/cmp.c
@@ -0,0 +1,9 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	alu_sub(opr_src.val, opr_dest.val, data_size);
+}
+
+make_instr_impl_2op(cmp, i, rm, bv);
