cb056031f2234b4cf28b18a24b9924860693f58c
monitor <tracer@njuics.org>
2018-09-29 22:58:01 +0800

    monitor test

---
+++ nemu/src/cpu/instr/ret.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(ret_near) {
+	OPERAND ret; // hold the return address
+	ret.type = OPR_MEM;
+	ret.addr = cpu.esp;
+	ret.data_size = data_size;
+
+	operand_read(&ret);
+	
+	cpu.esp -= (data_size / 8); // decrement esp
+	cpu.eip = ret.val;
+	
+	return 1;
+}
