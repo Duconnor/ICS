e799663b4c3f404ddb82f3bcb6af6a3833706c0e
tracer-pa2018-fall <tracer@njuics.org>
2018-09-29 22:05:54 +0800

    >  clean 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:05:54 up  1:04,  7 users,  load average: 0.02, 0.02, 0.00 3607a8aaf102757e2fe05766fb1cbc2a3348ddc

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,15 +4,16 @@ make_instr_func(call_near) {
 	opr_src.type = OPR_IMM;
 	opr_src.data_size = data_size;
 	opr_src.addr = eip + 1;
+	operand_read(&opr_src); // fetch the target procedure's address
 
-	operand_read(&opr_src);
-
+	// use opr_dest to implement push %eip
 	opr_dest.type = OPR_MEM;
 	cpu.esp -= (data_size / 8);
 	opr_dest.addr = cpu.esp;
 	opr_dest.val = cpu.eip;
-
 	operand_write(&opr_dest); // push old eip
+	
+	// change eip to the address of the target procedure
 	cpu.eip = opr_src.val;
 	
 	return 1 + data_size / 8;
