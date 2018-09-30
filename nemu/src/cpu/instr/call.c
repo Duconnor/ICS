d6bd51b6a454f7af34b067328df6aafb28deeec1
tracer-pa2018-fall <tracer@njuics.org>
2018-09-30 15:20:54 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:20:54 up  1:58,  5 users,  load average: 0.00, 0.00, 0.00 a01586a87ca225e4246b5d91f072187c68cf084

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -11,7 +11,7 @@ make_instr_func(call_near) {
 
 	int offset = sign_ext(rel.val, data_size);
 
-	// use opr_dest to implement push %eip
+	// use opr_dest to implement push eip
 	opr_dest.type = OPR_MEM;
 	cpu.esp -= (data_size / 8);
 	opr_dest.addr = cpu.esp;
