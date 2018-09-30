989562abbb208ccb1370505659561bb0b4d7d8d2
monitor <tracer@njuics.org>
2018-09-30 15:07:01 +0800

    monitor test

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -21,7 +21,5 @@ make_instr_func(call_near) {
 	// change eip to the address of the target procedure
 	cpu.eip += offset;
 
-	printf("%x\n", offset);
-
 	return 1 + data_size / 8;
 }
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -8,7 +8,6 @@ static void instr_execute_1op() {
 	opr_dest.val = opr_src.val;
 	opr_dest.data_size = opr_src.data_size;
 	operand_write(&opr_dest);
-	printf("%x%x%x%x\n", hw_mem[cpu.esp], hw_mem[cpu.esp - 1], hw_mem[cpu.esp - 2], hw_mem[cpu.esp - 3]);
 }
 
 make_instr_impl_1op(push, r, v)
