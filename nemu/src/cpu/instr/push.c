1d3ab852d36e5faa7240dcc8b6bbd057ed670c6f
monitor <tracer@njuics.org>
2018-09-30 12:32:01 +0800

    monitor test

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -6,6 +6,7 @@ static void instr_execute_1op() {
 	cpu.esp -= (opr_src.data_size / 8);
 	opr_dest.addr = cpu.esp;
 	opr_dest.val = opr_src.val;
+	opr_dest.data_size = opr_src.data_size;
 	operand_write(&opr_dest);
 }
 
