fe7752864abb2a0c561ef724902865def374e37b
monitor <tracer@njuics.org>
2018-09-30 11:57:01 +0800

    monitor test

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -3,8 +3,9 @@
 static void instr_execute_1op() {
 	operand_read(&opr_src);
 	opr_dest.type = OPR_MEM;
-	cpu.esp -= (data_size / 8);
+	cpu.esp -= (opr_src.data_size / 8);
 	opr_dest.addr = cpu.esp;
+	printf("%x\n", cpu.esp);
 	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
 }
