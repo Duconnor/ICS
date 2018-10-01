a7be3d5937c49bb4841c36bdeacbced8e5b05c62
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 17:14:29 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:14:29 up  2:13,  7 users,  load average: 0.02, 0.01, 0.00 61f72905a060b601e3dbc528153f651136591290

---
+++ nemu/src/cpu/instr/and.c
@@ -0,0 +1,12 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = opr_dest.val & opr_src.val;
+
+	cpu.eflags.CF = cpu.eflags.OF = 0;
+	cpu.eflags.SF = sf(opr_dest.val, opr_dest.data_size);
+	cpu.eflags.ZF = zf(opr_dest.val);
+	cpu.eflags.PF = pf(opr_dest.val);
+}
