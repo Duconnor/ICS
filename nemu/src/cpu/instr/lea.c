3a094e6fb2eac86147d6227b3c773e0b557a8061
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 16:05:00 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:05:00 up  1:04,  7 users,  load average: 0.00, 0.00, 0.00 f6642cc5a3fdf00dfe4a180246226f50ac0b8ef5

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,12 +6,11 @@ make_instr_func(lea) {
 	opr_dest.data_size = data_size;
 	// processing modrm byte to get src and dest operand
 	len += modrm_r_rm(eip + 1, &opr_dest, &opr_src);
-	operand_read(&opr_src);
 	
 	if (data_size < 32)
-		opr_dest.val = opr_src.val & 0xFFFF;
+		opr_dest.val = opr_src.addr & 0xFFFF;
 	else
-		opr_dest.val = opr_src.val;
+		opr_dest.val = opr_src.addr;
 
 	operand_write(&opr_dest);
 	print_asm_1("", "", "", &opr_dest);
