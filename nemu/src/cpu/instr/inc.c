f8fbaa59a753908e78922767fc9c5ad577d3d4c3
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 16:54:41 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:54:41 up  1:53,  7 users,  load average: 0.07, 0.02, 0.00 30e46de8c74dc45f4476d7058ff649e6d2f0a117

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,8 +1,10 @@
 #include "cpu/instr.h"
+#include <stdio.h>
 
 static void instr_execute_1op() {
 	operand_read(&opr_dest);
 	opr_dest.val = opr_dest.val + 1;
+	printf("%x\n", opr_dest.val)
 	operand_write(&opr_dest);
 }
 
