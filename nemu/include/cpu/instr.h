325acd3d2b14921242906b8d0de784ec6ead0937
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 10:25:08 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:25:08 up 7 min,  6 users,  load average: 0.02, 0.03, 0.00 bc15f99a1aad8fafda4040657df5ffaf31244e3

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -31,6 +31,7 @@ make_instr_func(jmp_near); // pre-defined
 make_instr_func(je_short_); // 0x74 pre-defined
 make_instr_func(jg_short_); // 0x7f pre-defined
 make_instr_func(jle_short_); // 0x7e pre-defined
+make_instr_func(jne_short_); // 0x75 pre-defined
 
 // add
 make_instr_func(add_i2rm_bv);
