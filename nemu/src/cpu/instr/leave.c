7e2f99086ae64f69554736e24eb5ce4b93a3a157
monitor <tracer@njuics.org>
2018-10-01 22:05:01 +0800

    monitor test

---
+++ nemu/src/cpu/instr/leave.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(leave) {
+	cpu.esp = cpu.ebp;
+	opr_src.type = OPR_MEM;
+	opr_src.sreg = SREG_CS;
+	opr_src.data_size = data_size;
+	opr_src.addr = cpu.esp;
+
+	operand_read(&opr_src);
+	cpu.esp += (data_size / 8);
+	cpu.ebp = opr_src.val;
+
+	return 1;
+}
