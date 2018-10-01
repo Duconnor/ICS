aa8c0065ec630132e0572aa5af239e19f05a3c1e
monitor <tracer@njuics.org>
2018-10-02 00:10:01 +0800

    monitor test

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -5,7 +5,7 @@ static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	alu_sub(opr_src.val, opr_dest.val, data_size);
-	printf("%x\n", opr_src.val);
+	printf("%x\n", opr_src.dest);
 }
 
 make_instr_impl_2op(cmp, i, rm, bv)
