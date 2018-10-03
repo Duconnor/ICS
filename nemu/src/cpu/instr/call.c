14040fdf89a7a61ae574611b33297f5430766ff4
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 20:44:23 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:44:23 up  3:20,  7 users,  load average: 0.00, 0.00, 0.00 bfd65b3bf2139defacc8826169f7906797cd557

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -17,7 +17,7 @@ make_instr_func(call_near) {
 	opr_dest.addr = cpu.esp;
 	eip = eip + 1 + (data_size / 8); // set eip to the next instr
 	opr_dest.val = eip;
-	opr_dest.data_size = data_size;
+	opr_dest.data_size = 32; // eip is always 32
 	operand_write(&opr_dest); // push old eip
 
 	// change eip to the address of the target procedure
