6ae3f746f5764b1d6787f36493d793ade191436d
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 19:40:29 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:40:29 up  2:16,  7 users,  load average: 0.10, 0.05, 0.01 31a8dbfe7f286426f98c261713c67712c0f83d06

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -4,7 +4,7 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	alu_sub(opr_src.val, sign_ext(opr_dest.val, data_size), data_size);
+	alu_sub(sign_ext(opr_src.val, data_size), opr_dest.val, data_size);
 }
 
 make_instr_impl_2op(cmp, i, rm, bv)
