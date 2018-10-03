7a772da8aa3b2112efa868c7999e6ee3150e31d6
monitor <tracer@njuics.org>
2018-10-03 17:18:01 +0800

    monitor test

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -1,11 +1,9 @@
 #include "cpu/instr.h"
-#include <stdio.h>
 
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
-	printf("mov!\n");
 }
 
 make_instr_impl_2op(mov, r, rm, b)
