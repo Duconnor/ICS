32d0a1793437ac5a14e60459f343165a03747210
monitor <tracer@njuics.org>
2018-10-03 11:43:01 +0800

    monitor test

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -51,6 +51,19 @@ make_instr_func(jl_near); // 0x0f 8c
 make_instr_func(jge_near); // 0x0f 8d
 make_instr_func(jle_near); // 0x0f 8e
 make_instr_func(jg_near); // 0xf 8f
+make_instr_func(jo_short_); // 0x70
+make_instr_func(jno_short_); // 0x71
+make_instr_func(jb_short_); // 0x72
+make_instr_func(jae_short_); // 0x73
+make_instr_func(ja_short_); // 0x77
+make_instr_func(js_short_); // 0x78
+make_instr_func(jns_short_); // 0x79
+make_instr_func(jp_short_); // 0x7a
+make_instr_func(jnp_short_); // 0x7b
+make_instr_func(jl_short_); // 0x7c
+make_instr_func(jge_short_); // 0x7d
+make_instr_func(jle_short_); // 0x7e
+make_instr_func(jg_short_); // 0x7f
 
 // add
 make_instr_func(add_i2rm_bv);
