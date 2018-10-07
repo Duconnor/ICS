39569fbbdc8266e61805dc80bf4e302702cd6a2d
tracer-pa2018-fall <tracer@njuics.org>
2018-10-07 11:36:23 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:36:23 up 25 min,  6 users,  load average: 0.02, 0.01, 0.00 91c7394a1048974f5c8898fb674beb0f70180f48

--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -5,7 +5,7 @@ static void instr_execute_2op() {
 	operand_read(&opr_dest);
 	if (opr_src.data_size == 8 && opr_dest.data_size > 8)
 		opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
-	opr_dest = alu_sbb(opr_src.val, opr_dest.val, data_size);
+	opr_dest.val = alu_sbb(opr_src.val, opr_dest.val, data_size);
 	operand_write(&opr_dest);
 }
 
