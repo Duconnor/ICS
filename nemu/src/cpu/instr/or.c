4f7b546619eb5b117aa6626057159668c625b1f1
monitor <tracer@njuics.org>
2018-10-07 11:59:01 +0800

    monitor test

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -3,6 +3,8 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	if (opr_src.data_size == 8 && opr_dest.data_size > 8)
+		opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
 	opr_dest.val = opr_dest.val | opr_src.val;
 	operand_write(&opr_dest);
 
