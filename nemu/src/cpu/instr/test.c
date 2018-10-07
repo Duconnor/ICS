a7efd169a26eaffdb1f9f28f292541dcfc6cd0f1
tracer-pa2018-fall <tracer@njuics.org>
2018-10-07 20:00:12 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:00:12 up 23 min,  6 users,  load average: 0.04, 0.05, 0.01 214dd44a3702aefdaa1c327bbb547fdc14b904ae

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -4,7 +4,6 @@ static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest.val = opr_src.val & opr_dest.val;
-	operand_write(&opr_dest);
 
 	// two eflags need to be set
 	cpu.eflags.CF = 0;
