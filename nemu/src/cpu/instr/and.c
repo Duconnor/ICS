37a26fa191283c1f5f56e64e6e43e19f9e77ee27
monitor <tracer@njuics.org>
2018-10-07 11:30:01 +0800

    monitor test

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -3,10 +3,11 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	// you got to perform sign extend when src val is a byte
 	if (opr_src.data_size == 8)
 		opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
 	opr_dest.val = opr_dest.val & opr_src.val;
-	operand_write(&opr_dest); // magic code that destroy quick-sort
+	operand_write(&opr_dest);
 
 	cpu.eflags.CF = cpu.eflags.OF = 0;
 	cpu.eflags.SF = sf(opr_dest.val, opr_dest.data_size);
