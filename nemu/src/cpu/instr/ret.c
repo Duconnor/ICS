7a7e2a6f264798ea223ad3f839e25ef9d937c959
tracer-pa2018-fall <tracer@njuics.org>
2018-10-07 19:48:39 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:48:39 up 11 min,  5 users,  load average: 0.01, 0.03, 0.00 c7d4e7c8b4234fce39a7e3e7c823f4b0de45f4e2

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,13 +5,44 @@ make_instr_func(ret_near) {
 	ret.type = OPR_MEM;
 	ret.sreg = SREG_CS;
 	ret.addr = cpu.esp;
-	ret.data_size = 32;
+	ret.data_size = data_size;
 
 	operand_read(&ret);
 	
 	cpu.esp += (data_size / 8); // increment esp
 	cpu.eip = ret.val;
-	print_asm_0("ret", "", 2);
+	if (data_size == 16)
+		cpu.eip &= 0xffff;
+	print_asm_0("ret", "", 1);
 	
+	return 0;
+}
+
+make_instr_func(ret_near_imm16) {
+	OPERAND ret;
+	ret.type = OPR_MEM;
+	ret.sreg = SREG_CS;
+	ret.addr = cpu.esp;
+	ret.data_size = data_size;
+
+	operand_read(&ret);
+
+	cpu.esp += (data_size / 8);
+	cpu.eip = ret.val;
+	if (data_size == 16)
+		cpu.eip &= 0xffff;
+	print_asm_0("ret", "", 1);
+
+	// deal with the imm16
+	OPERAND imm16;
+	imm16.type = OPR_MEM;
+	imm16.sreg = SREG_CS;
+	imm16.addr = eip + 1;
+	imm16.data_size = 16;
+
+	operand_read(&imm16);
+
+	cpu.esp += imm16.val;
+
 	return 0;
 }
