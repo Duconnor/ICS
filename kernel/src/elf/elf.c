1c6961a643add6453e26ed8f6ebe8999955036ca
monitor <tracer@njuics.org>
2018-10-15 20:09:01 +0800

    monitor test

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,7 +40,7 @@ uint32_t loader() {
 			memcpy(hw_mem + ph->p_vaddr, elf + ph->p_offset, ph->p_filesz);
 
 			/* zero the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			memcpy(hw_mem + ph->p_vaddr + p->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memcpy(hw_mem + ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
