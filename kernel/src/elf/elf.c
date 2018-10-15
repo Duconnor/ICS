6675754a36a3f6c867d8bf086ac9e35a165c87a6
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 20:21:56 +0800

    >  clean 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:21:56 up 32 min,  2 users,  load average: 0.08, 0.03, 0.01 cd06ce23bc7bf09a8a0830934baa08318d394996

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,10 +37,10 @@ uint32_t loader() {
 			// panic("Please implement the loader");
 
 			/* copy the segment from the ELF file to its proper memory area */
-			memcpy(ph->p_vaddr, elf + ph->p_offset, ph->p_filesz);
+			memcpy(elf + ph->p_vaddr, elf + ph->p_offset, ph->p_filesz);
 
 			/* zero the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			memcpy(ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memcpy(elf + ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
