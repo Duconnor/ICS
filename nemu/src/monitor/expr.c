8776a0251856c817fc66ab9b9eabfa3ff68f97af
monitor <tracer@njuics.org>
2018-10-17 15:20:01 +0800

    monitor test

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -30,11 +30,6 @@ static struct rule {
 	{"\\+", '+'},
 	{"\\-", '-'},
 	{"==", EQ},
-	{"[0-9]+", NUM},
-	{"$e[a,c,d,b]x", REG},
-	{"$esp", REG},
-	{"$esi", REG},
-	{"[a-zA-Z]+[a-zA-Z0-9]*", SYMB},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
