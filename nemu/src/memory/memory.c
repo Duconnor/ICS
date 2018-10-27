ad3f6f837ab73d84d734e5066d1b126a73d6008b
tracer-pa2018-fall <tracer@njuics.org>
2018-10-27 20:15:45 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:15:45 up 44 min,  4 users,  load average: 0.00, 0.01, 0.00 535946eac009a7452e901895260e227ce9e6789

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -11,6 +11,7 @@ uint8_t hw_mem[MEM_SIZE_B];
 uint32_t hw_mem_read(paddr_t paddr, size_t len) {
 	uint32_t ret = 0;
 	memcpy(&ret, hw_mem + paddr, len);
+	printf("val: %d\t%d", paddr, hw_mem[paddr]);
 	return ret;
 }
 
