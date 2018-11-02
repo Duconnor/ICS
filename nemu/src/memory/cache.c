749d8c3f29026e3339cd827c894d09d2239c96c5
monitor <tracer@njuics.org>
2018-11-02 19:56:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,1 +1,11 @@
+#include "cache.h"
+
 CacheLine[1024]; // Cache has 64KB
+
+void init_cache() {
+	memset(CacheLine, 0, sizeof(CacheLine));
+}
+
+uint32_t cache_read(paddr_t paddr, size_t len) {
+	
+}
