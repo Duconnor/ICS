671b8b4f3e09dee571e7b42d6b46893ba9173e14
monitor <tracer@njuics.org>
2018-11-02 19:53:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -3,3 +3,9 @@ struct CacheLine {
 	unsigned int flag_bit:19; // q = 7, b = 6, so 32 - 7 - 6 = 19
 	unsigned char slot[64]; // cache slot
 };
+
+void init_cache(); // initial cache
+
+uint32_t cache_read(paddr_t paddr, size_t len); // read from cache
+
+void cache_write(paddr_t paddr, size_t len, uint32_t data); // write to cache
