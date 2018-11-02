96d9faf84216754312d8014843b321f1d26894a3
monitor <tracer@njuics.org>
2018-11-02 20:40:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,11 +1,68 @@
-#include "cache.h"
+#include "memory/cache.h"
+#include <stdlib.h>
 
-CacheLine[1024]; // Cache has 64KB
+// 8-way set associative
+#define WAYNUM 8
+
+CacheLine cache[1024]; // Cache has 64KB
 
 void init_cache() {
-	memset(CacheLine, 0, sizeof(CacheLine));
+	memset(cache, 0, sizeof(CacheLine));
 }
 
 uint32_t cache_read(paddr_t paddr, size_t len) {
-	
+	// b = 6, q = 7, rest = 19
+	assert(len == 1 || len == 2 || len == 4 || len == 3);
+	uint32_t flag = (paddr >> 13) & 0x7FFFF;
+	uint32_t group_index = ((paddr >> 6) & 0x7F) * WAYNUM;
+	uint32_t address_inside_group = paddr & 0x3F;
+	uint32_t data = 0;
+	int flag = 0, empty_line = -1;
+
+	// begin scan and read
+	for (int i = 0; i < WAYNUM; i++) {
+		int line_num = group_index + i;
+		if (cache[line_num].valid_bit == 1) {
+			// valid!
+			if (cache[line_num].flag_bits == flag) {
+				// flag bits meet
+				flag = 1;
+				for (int j = 0; j < len; j++) {
+					data <<= 8;
+					if (address_inside_group + j >= 64) {
+						// cases when read cross the line
+						// recursively call cache_read function
+						uint32_t new_address = paddr + j; // j bytes have been read, so increase address by j
+						uint32_t data_rest = cache_read(new_address, len - j);
+						data <<= (len - j - 1) * 8; // because we have already shift left before, so remember minus 1 here
+						data |= data_rest;
+						return data;
+					}
+					data |= cache[line_num].slot[address_inside_group + j];
+				}
+			}
+		} else {
+			empty_line = line_num;
+		}
+	}
+
+	if (flag == 0) {
+		// the data we are looking for is not in the cache
+		// 1. get the data from memory
+		for (int i = 0; i < len; i++) {
+			data <<= 8;
+			data |= hw_mem[paddr + i];
+		}
+		// 2. write the whole chunck of data into the cache
+		uint32_t start_address = paddr & 0xC0;
+		if (empty_line == -1) {
+			// randomly replace one
+			int replace_line = (rand() % WAYNUM) + group_index;
+			memcpy(cache[replace_line].slow, hw_mem + start_address, 64);
+		} else {
+			// there is an empty cache line
+			memcpy(cache[empty_line].slot, hw_mem + start_address, 64);
+		}
+	}
+	return data;
 }
