21911460515ddfd60810010007cfbac8289eb79f
monitor <tracer@njuics.org>
2018-11-02 19:58:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,6 +1,6 @@
 struct CacheLine {
 	unsigned char valid_bit:1; // 1 valid bit
-	unsigned int flag_bit:19; // q = 7, b = 6, so 32 - 7 - 6 = 19
+	uint32_t flag_bit:19; // q = 7, b = 6, so 32 - 7 - 6 = 19
 	unsigned char slot[64]; // cache slot
 };
 
