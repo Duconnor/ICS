b4984086c5ed5184d9ba974029f0f6e6132f545b
tracer-pa2018-fall <tracer@njuics.org>
2018-11-03 17:54:54 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:54:54 up 25 min,  2 users,  load average: 0.07, 0.04, 0.00 33f95d70240b042c3b2d804ac2dd0d3467c2f923

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -52,7 +52,7 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 		}
 	}
 
-	printf("empty_line: %d", empty_line);
+	printf("empty_line: %d\n", empty_line);
 
 	if (hit == 0) {
 		// the data we are looking for is not in the cache
