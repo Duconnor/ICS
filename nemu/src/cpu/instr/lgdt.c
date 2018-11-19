d98e6abd710fa517aed9127a9efaaae8f49894a8
monitor <tracer@njuics.org>
2018-11-19 17:46:02 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,1 +1,16 @@
 #include "cpu/instr.h"
+
+make_instr_func(lgdt) {
+	OPERAND limit, base;
+	limit.type = OPR_MEM;
+	base.type = OPR_MEM;
+
+	limit.data_size = 16;
+	if (data_size == 16)
+		base.data_size = 24;
+	else
+		base.data_size = 32;
+
+	limit.addr = eip + 1;
+	// TODO: determine the proper address of base
+} 
