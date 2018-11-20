68d19393abb72cd205ab710c3444126fa26a65af
tracer-pa2018-fall <tracer@njuics.org>
2018-11-20 21:37:08 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:37:08 up  1:35,  5 users,  load average: 0.00, 0.00, 0.00 2eafd2aedad002fb37d779cddb61468fef5aced7

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -51,3 +51,23 @@ make_instr_func(jmp_near_indirect) {
 
 	return 0;
 }
+
+make_instr_func(jmp_far_imm) {
+	OPERAND ptr16, ptr32;
+	ptr16.type = ptr32.type = OPR_IMM;
+	ptr16.data_size = 16;
+	ptr32.data_size = 32;
+
+	ptr16.addr = eip + 1;
+	ptr32.addr = eip + 3;
+
+	operand_read(&ptr16);
+	operand_read(&ptr32);
+
+	cpu.cs.val = ptr16.val;
+	cpu.eip = ptr32;
+
+	load_sreg(1); // ljmp must be cs
+
+	return 7;
+}
