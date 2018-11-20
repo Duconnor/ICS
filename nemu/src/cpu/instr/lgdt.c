24f12ac0422236c3e3737b5a52b92d0bcf62a817
tracer-pa2018-fall <tracer@njuics.org>
2018-11-20 20:22:24 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:22:24 up 20 min,  4 users,  load average: 0.02, 0.02, 0.00 dff057dc3815205e9bb344642623df8247b37fea

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,9 +3,9 @@
 make_instr_func(lgdt) {
 	uint32_t address = 0;
 	address |= paddr_read(eip + 2, 1);
-	address |= paddr_read(eip + 2, 1) << 8;
-	address |= paddr_read(eip + 2, 1) << 16;
-	address |= paddr_read(eip + 2, 1) << 24;
+	address |= paddr_read(eip + 3, 1) << 8;
+	address |= paddr_read(eip + 4, 1) << 16;
+	address |= paddr_read(eip + 5, 1) << 24;
 
 	cpu.gdtr.limit |= paddr_read(address, 1);
 	cpu.gdtr.limit |= paddr_read(address + 1, 1) << 8;
