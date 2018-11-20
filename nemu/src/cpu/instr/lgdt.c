9b6d047714c81cb7ed7ddc8c14f138f63db3e06b
monitor <tracer@njuics.org>
2018-11-20 20:17:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,4 +6,12 @@ make_instr_func(lgdt) {
 	address |= paddr_read(eip + 2, 1) << 8;
 	address |= paddr_read(eip + 2, 1) << 16;
 	address |= paddr_read(eip + 2, 1) << 24;
+
+	cpu.gdtr.limit |= paddr_read(address, 1);
+	cpu.gdtr.limit |= paddr_read(address + 1, 1) << 8;
+
+	cpu.gdtr.base |= paddr_read(address + 2, 1);
+	cpu.gdtr.base |= paddr_read(address + 3, 1) << 8;
+	cpu.gdtr.base |= paddr_read(address + 4, 1) << 16;
+	cpu.gdtr.base |= paddr_read(address + 5, 1) << 24;
 }
