d255abff8cdde599fc59a423b9d158c43a061401
tracer-pa2018-fall <tracer@njuics.org>
2018-11-20 20:52:47 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:52:47 up 50 min,  5 users,  load average: 0.00, 0.00, 0.00 df4c16b09bf531195920c22f85037ac831f4261

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,10 +2,19 @@
 
 make_instr_func(lgdt) {
 	OPERAND limit, base;
-	limit.type = 
+	limit.type = OPR_IMM;
+	base.type = OPR_IMM;
 	limit.data_size = 16;
 	base.data_size = 32;
 
 	limit.addr = eip + 3;
 	base.addr = eip + 3;
+
+	operand_read(&limit);
+	operand_read(&base);
+
+	cpu.gdtr.base = base.val;
+	cpu.gdtr.limit = limit.val;
+
+	return 7;
 }
