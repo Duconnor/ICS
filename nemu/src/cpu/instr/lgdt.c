8e586c230a012765fb1615966dcc4f7ce2385f22
monitor <tracer@njuics.org>
2018-11-20 20:14:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,16 +1,9 @@
 #include "cpu/instr.h"
 
 make_instr_func(lgdt) {
-	OPERAND limit, base;
-	limit.type = OPR_MEM;
-	base.type = OPR_MEM;
-
-	limit.data_size = 16;
-	if (data_size == 16)
-		base.data_size = 24;
-	else
-		base.data_size = 32;
-
-	limit.addr = eip + 1;
-	// TODO: determine the proper address of base
-} 
+	uint32_t address = 0;
+	address |= paddr_read(eip + 2, 1);
+	address |= paddr_read(eip + 2, 1) << 8;
+	address |= paddr_read(eip + 2, 1) << 16;
+	address |= paddr_read(eip + 2, 1) << 24;
+}
