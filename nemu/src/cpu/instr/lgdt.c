0ad92439c4d0a4d3f3074b0151025befa59be7ee
monitor <tracer@njuics.org>
2018-11-20 20:51:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,19 +1,9 @@
 #include "cpu/instr.h"
 
 make_instr_func(lgdt) {
-	uint32_t address = 0;
-	address |= paddr_read(eip + 3, 1);
-	address |= paddr_read(eip + 4, 1) << 8;
-	address |= paddr_read(eip + 5, 1) << 16;
-	address |= paddr_read(eip + 6, 1) << 24;
+	OPERAND limit, base;
+	limit.data_size = 16;
+	base.data_size = 32;
 
-	cpu.gdtr.limit |= paddr_read(address, 1);
-	cpu.gdtr.limit |= paddr_read(address + 1, 1) << 8;
-
-	cpu.gdtr.base |= paddr_read(address + 2, 1);
-	cpu.gdtr.base |= paddr_read(address + 3, 1) << 8;
-	cpu.gdtr.base |= paddr_read(address + 4, 1) << 16;
-	cpu.gdtr.base |= paddr_read(address + 5, 1) << 24;
-
-	return 7; // 6-byte long
+	limit.addr
 }
