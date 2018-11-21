fc9b6ffec3e11ca574c565427884e083fbf25969
monitor <tracer@njuics.org>
2018-11-21 16:53:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -14,4 +14,21 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+	
+	// ignore ti and rpl here
+	// first, get the index and the gdt
+	uint32_t index = cpu.segReg[sreg].selector.index;
+	uint32_t gdt = cpu.gdtr.base;
+
+	// find the corresponding segment descriptor
+	SegDesc segDesc;
+	segDesc.val[0] = paddr_read(gdt + index * 8, 4);
+	segDesc.val[1] = paddr_read(gdt + index * 8 + 4, 4);
+
+	// load
+	cpu.segReg[sreg].invisible.base = segDesc.base_31_24 << 24 | segDesc.base_23_16 << 16 | segDesc.base_15_0;
+	cpu.segReg[sreg].invisible.limit = segDesc.limit_19_16 << 16 | segDesc.limit_15_0;
+	cpu.segReg[sreg].invisible.granularity = segDesc.granularity;
+	cpu.segReg[sreg].invisible.present = segDesc.present;
+	cpu.segReg[sreg].invisible.privilege_level = segDesc.privilege_level;
 }
