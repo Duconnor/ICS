71b7cad801568ceb89c6827ac09bcc485dcdb304
monitor <tracer@njuics.org>
2018-11-21 16:59:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -6,8 +6,7 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	
-	return 0;
+	return offset + cpu.segReg[sreg].invisible.base;
 }
 
 // load the invisible part of a segment register
