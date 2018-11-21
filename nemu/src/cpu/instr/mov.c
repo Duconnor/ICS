26a8d2f116471ea29400641c87a37a98e2b4bce4
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 16:31:32 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:31:32 up 52 min,  4 users,  load average: 0.00, 0.00, 0.00 1dc5fbedd18004837440f0387be94b2da4844dd

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -41,6 +41,7 @@ make_instr_func(mov_r2c_l) {
 	modrm_c_r(eip + 2, &dest, &src);
 	operand_read(&src);
 	dest.val = src.val;
+	printf("addr:%d", dest.addr);
 	operand_write(&dest);
 	load_sreg(dest.addr);
 	return 2;
