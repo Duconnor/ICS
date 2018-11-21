dc19e51e390a00f12bd755844a8eac7956e5d12e
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 16:28:13 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:28:13 up 49 min,  4 users,  load average: 0.00, 0.00, 0.00 70a9d2787d736a3fd0bca3e836851ad32fb10fc8

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -30,7 +30,7 @@ make_instr_func(mov_c2r_l) {
 	dest.val = src.val;
 	operand_write(&dest);
 	//printf("here2\n");
-	return 3;
+	return 2;
 }
 
 make_instr_func(mov_r2c_l) {
@@ -43,7 +43,7 @@ make_instr_func(mov_r2c_l) {
 	dest.val = src.val;
 	operand_write(&dest);
 	load_sreg(dest.addr);
-	return 3;
+	return 2;
 }
 
 /*
