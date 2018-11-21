bf9826c1f12560417b0bb972c7efbbc8473f931f
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 17:04:16 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:04:16 up  1:25,  5 users,  load average: 0.19, 0.09, 0.03 79be5885daeaa4f365c6991d921b3fedd0147552

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -53,7 +53,7 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len) {
 	return laddr_read(vaddr, len);
 #else
 	uint32_t laddr = vaddr;
-	if (cpu.cr0.pe == 1) {
+	if (cpu.cr0.PE == 1) {
 		laddr = segment_translate(vaddr, sreg);
 	}
 	return laddr_read(laddr, len);
@@ -66,7 +66,7 @@ void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data) {
 	laddr_write(vaddr, len, data);
 #else
 	uint32_t laddr = vaddr;
-	if (cpu.cr0.pe == 1) {
+	if (cpu.cr0.PE == 1) {
 		laddr = segment_translate(vaddr, sreg);
 	}
 	return laddr_write(laddr, len, data);
