0f1d4683cd81a1df9950996207c9f7ec8957632a
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 18:17:39 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  18:17:39 up  2:39,  5 users,  load average: 0.00, 0.00, 0.00 f0234a710e77ce271047d14d97d90a7407330e4

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -18,23 +18,23 @@ void load_sreg(uint8_t sreg) {
 	// ignore ti and rpl here
 	// first, get the index and the gdt
 	uint32_t index = cpu.segReg[sreg].selector.index;
-	printf("%x\n", cpu.segReg[sreg].selector.index);
+	//printf("%x\n", cpu.segReg[sreg].selector.index);
 	uint32_t gdt = cpu.gdtr.base;
 
 	// find the corresponding segment descriptor
 	SegDesc segDesc;
 	segDesc.val[0] = paddr_read(gdt + index * 8, 4);
-	printf("%x\n", segDesc.val[0]);
+	//printf("%x\n", segDesc.val[0]);
 	segDesc.val[1] = paddr_read(gdt + index * 8 + 4, 4);
-	printf("%x\n", segDesc.val[1]);
+	//printf("%x\n", segDesc.val[1]);
 
 	// load
 	cpu.segReg[sreg].invisible.base = segDesc.base_31_24 << 24 | segDesc.base_23_16 << 16 | segDesc.base_15_0;
 	cpu.segReg[sreg].invisible.limit = segDesc.limit_19_16 << 16 | segDesc.limit_15_0;
-	printf("%x\n", segDesc.base_15_0);
+	//printf("%x\n", segDesc.base_15_0);
 	cpu.segReg[sreg].invisible.granularity = segDesc.granularity;
 	cpu.segReg[sreg].invisible.present = segDesc.present;
 	cpu.segReg[sreg].invisible.privilege_level = segDesc.privilege_level;
-	printf("gdt: %x\n", cpu.segReg[sreg].invisible.limit);
+	printf("gdt: %x\n", cpu.segReg[sreg].invisible.base);
 	assert(cpu.segReg[sreg].invisible.base == 0 && cpu.segReg[sreg].invisible.limit == 0x7FFFF && cpu.segReg[sreg].invisible.granularity == 1 && cpu.segReg[sreg].invisible.present == 1);
 }
