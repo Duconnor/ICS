003c95beb908d64517b5307e6592b8b0fb2f9b0d
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 17:49:42 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:49:42 up  2:11,  5 users,  load average: 0.00, 0.00, 0.00 84946bb525646598f892a3fa97cac194f152980f

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -10,7 +10,7 @@ make_instr_func(lgdt) {
 	uint32_t gdt = paddr_read(gdtaddr.val, 4);
 
 	cpu.gdtr.limit = gdt & 0xFFFF;
-	cpu.gdtr.base = gdt;
+	cpu.gdtr.base = (gdt && 0xFFFF0000) >> 16;
 
 	return 6;
 }
