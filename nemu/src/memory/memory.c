d81d6242361d556af38381a7e458ed0d7d434bbe
tracer-pa2018-fall <tracer@njuics.org>
2018-11-27 21:56:19 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:56:19 up  2:32,  6 users,  load average: 0.09, 0.13, 0.09 e703b6c2b160857414066267d046f4c14792f0bd

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -48,14 +48,14 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 #else
 	//printf("%x\n", cpu.cr0.val);
 	if (cpu.cr0.PE == 1 && cpu.cr0.paging == 1) {
-		//printf("here\n");
+		printf("here\n");
 		uint8_t start = (laddr >> 12) & 0x1;
 		uint8_t end = ((laddr + len) >> 12) & 0x1;
 		if (start != end) {
 			assert(0);
 		} else {
 			paddr_t paddr = page_translate(laddr);
-			printf("%x\n", paddr);
+			//printf("%x\n", paddr);
 			return paddr_read(paddr, len);
 		}
 	} else {
