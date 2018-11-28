55351288746b5c0431ade29b01f3a8a50493f973
tracer-pa2018-fall <tracer@njuics.org>
2018-11-28 14:39:19 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:39:19 up 31 min,  3 users,  load average: 0.00, 0.00, 0.00 8b37a868c3736d78bc60765f2ed3101c741da412

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -11,7 +11,8 @@ paddr_t page_translate(laddr_t laddr) {
 	uint32_t dir = laddr >> 22; // 10-bit
 	uint32_t page = (laddr >> 12) & 0x3FF; // 10-bit
 	uint32_t offset = laddr & 0xFFF; // 12-bit
-	uint32_t dirbase = cpu.cr3.page_directory_base;
+	//uint32_t dirbase = cpu.cr3.page_directory_base;
+	uint32_t dirbase = 0x73;
 	// read the page directory entry
 	PDE dir_entry;
 	//printf("%x\n", (uint32_t)ptr);
