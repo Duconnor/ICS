eec1805fb1ddeadb0a9b103ae7f59eedd57418fc
tracer-pa2018-fall <tracer@njuics.org>
2018-11-28 14:46:37 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:46:37 up 38 min,  3 users,  load average: 0.00, 0.00, 0.00 778db38faaf2d055cddc8900d366c26f47bb08a6

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -45,6 +45,7 @@ void init() {
 
 /* Initialization phase 2 */
 void init_cond() {
+	BREAK_POINT;
 #ifdef IA32_INTR
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
