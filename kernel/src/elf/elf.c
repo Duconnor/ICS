77198210835c36b38dc482316955f521ff4017a9
tracer-pa2018-fall <tracer@njuics.org>
2018-12-10 11:42:38 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:42:38 up  3:24,  6 users,  load average: 0.03, 0.06, 0.01 bc71b9e8471ad1c05fd2d0b5ad5fe48c94ffdf65

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,7 @@ uint32_t loader() {
 			char* mem = (char*)(0x0 + ph->p_vaddr);
 #else
 			char* mem = (char*)mm_malloc(ph->p_vaddr, ph->p_memsz);
-			//Log("%x\n", mem);
+			Log("%x", mem);
 #endif
 			memcpy(mem, (char*)ph->p_offset, ph->p_filesz);
 
