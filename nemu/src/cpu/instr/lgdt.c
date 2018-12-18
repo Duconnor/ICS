3f8e78c1d5e0c10742ea70a5a0780ae5134fcc4a
tracer-pa2018-fall <tracer@njuics.org>
2018-12-18 22:38:45 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:38:45 up  6:11,  7 users,  load average: 0.01, 0.00, 0.00 31bc267bd5dc1dde86941a7a626e0b4cfd13e401

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,18 +3,23 @@
 
 make_instr_func(lgdt) {
 	OPERAND gdtaddr;
-	gdtaddr.data_size = 32;
+	//gdtaddr.data_size = 32;
 	uint32_t len = modrm_rm(eip + 1, &gdtaddr);
 	//printf("%x\n", gdtaddr.type);
 	//gdtaddr.type = OPR_IMM;
 	//gdtaddr.addr = eip + 2;
 
-	operand_read(&gdtaddr);
+	//operand_read(&gdtaddr);
 	//printf("%x\n", gdtaddr.val);
 
 	//cpu.gdtr.limit = paddr_read(gdtaddr.val, 2);
 	//cpu.gdtr.base = paddr_read(gdtaddr.val + 2, 4);
-	cpu.gdtr.limit = gdtaddr.val & 0xFFFF;
+	gdtaddr.data_size = 16;
+	operand_read(&gdtaddr);
+	cpu.gdtr.limit = gdtaddr.val;
+	gdtaddr.data_size = 32;
+	gdtaddr.addr += 2;
+	operand_read(&gdtaddr);
 	cpu.gdtr.base = gdtaddr.val;
 
 
