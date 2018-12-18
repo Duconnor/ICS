e141641def2274faf58d61d40e51d262e9668995
tracer-pa2018-fall <tracer@njuics.org>
2018-12-18 10:33:50 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:33:50 up 22 min,  5 users,  load average: 0.02, 0.04, 0.09 45fea59860ac62af6b92902cbe0e25cb9fa842d0

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -5,6 +5,7 @@ make_instr_func(lgdt) {
 	OPERAND gdtaddr;
 	gdtaddr.data_size = 32;
 	uint32_t len = modrm_rm(eip + 2, &gdtaddr);
+	printf("here\n");
 	if (gdtaddr.type == OPR_IMM)
 		printf("IMM\n");
 	//gdtaddr.type = OPR_IMM;
