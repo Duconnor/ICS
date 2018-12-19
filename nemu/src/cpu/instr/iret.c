963b74d188ab5360465dfdd90f98f943ca759d22
monitor <tracer@njuics.org>
2018-12-19 20:20:01 +0800

    monitor test

---
+++ nemu/src/cpu/instr/iret.c
@@ -0,0 +1,23 @@
+#include "cpu/instr.h"
+
+static uint32_t pop(uint8_t len) {
+	OPERAND ele;
+	ele.type = OPR_MEM;
+	ele.sreg = SREG_CS;
+	ele.data_size = len;
+	ele.addr = cpu.esp;
+
+	operand_read(&ele);
+	cpu.esp += (len / 8);
+
+	return ele.val;
+}
+
+make_instr_func(iret) {
+	cpu.eip = pop(32);
+	cpu.cs.val = pop(16);
+	cpu.cs.invi_val2 = pop(32);
+	cpu.cs.invi_val1 = pop(32);
+	cpu.eflags.val = pop(32);
+	return 1;	
+}
