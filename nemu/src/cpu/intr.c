18c09dddc8e6aeab2f2c73e1717301f59f48d1f7
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 12:45:08 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:45:08 up 10:25, 13 users,  load average: 0.03, 0.02, 0.00 bf32e0d14e6313d4ddff09fe1fac802bd6e92840

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -33,8 +33,8 @@ void raise_intr(uint8_t intr_no) {
 	uint32_t base = cpu.idtr.base;
 	GateDesc desc;
 	printf("base: %x\n", base);
-	desc.val[0] = vaddr_read(base + intr_no * 8, SREG_CS, 4);
-	desc.val[1] = vaddr_read(base + intr_no * 8 + 4, SREG_CS ,4);
+	desc.val[0] = vaddr_read(base + intr_no * 8, SREG_DS, 4);
+	desc.val[1] = vaddr_read(base + intr_no * 8 + 4, SREG_DS ,4);
 	uint32_t offset = desc.offset_31_16 << 16 | desc.offset_15_0;
 	uint32_t addr = vaddr_read(offset, SREG_CS, 4);
 
