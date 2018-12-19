197356a15f9f680e7cde83a2d90dfb7b6ce4c67e
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 12:46:03 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:46:03 up 10:26, 13 users,  load average: 0.01, 0.02, 0.00 545f485adf0c9291fd02dc2e20ab9bde4c08528

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -34,8 +34,8 @@ void raise_intr(uint8_t intr_no) {
 	GateDesc desc;
 	desc.val[0] = vaddr_read(base + intr_no * 8, SREG_DS, 4);
 	desc.val[1] = vaddr_read(base + intr_no * 8 + 4, SREG_DS ,4);
-	printf("base: %x\n", base);
 	uint32_t offset = desc.offset_31_16 << 16 | desc.offset_15_0;
+	printf("offset: %x\n", offset);
 	uint32_t addr = vaddr_read(offset, SREG_CS, 4);
 
 	// clear IF if it is an interrupt
