fcb5378934b5c8f376af3b83e550ed74b234747d
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 12:33:44 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:33:44 up 10:14, 14 users,  load average: 0.00, 0.00, 0.00 371d618a90b89c73816618fa5a8da935dbedcaaa

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,11 +2,48 @@
 #include "cpu/instr.h"
 #include "memory/memory.h"
 
+static void push(uint32_t val, uint8_t len) {
+	if (len == 16)
+		val &= 0xFFFF;
+	OPERAND ele;
+	ele.type = OPR_MEM;
+	cpu.esp -= (len / 8);
+	ele.addr = cpu.esp;
+	ele.val = sign_ext(val, len);
+	ele.data_size = len;
+	ele.sreg = SREG_CS;
+	operand_write(&ele);
+}
 
 void raise_intr(uint8_t intr_no) {
 #ifdef IA32_INTR
+	/*
 	printf("Please implement raise_intr()");
 	assert(0);
+	*/
+	
+	// push EFLAGS, CS and EIP
+	push(cpu.eflags.val, 32);
+	push(cpu.cs.invi_val1, 32);
+	push(cpu.cs.invi_val2, 32);
+	push(cpu.cs.invi_val, 16);
+	push(cpu.eip, 32);
+
+	// find the IDT entry
+	uint32_t base = idtr.base;
+	GateDesc desc;
+	desc.val[0] = paddr_read(base + intr_no * 8, 4);
+	desc.val[1] = paddr_read(base + intr_no * 8 + 4, 4);
+	uint32_t offset = desc.offset_31_16 << 16 | desc.offset_15_0;
+	uint32_t addr = vaddr_read(offset, SREG_CS, 4);
+
+	// clear IF if it is an interrupt
+	if (intr_no != 0x80)
+		cpu.eflags.IF = 0;
+
+	// set eip to the interrupt handler
+	cpu.eip = addr;
+
 #endif
 }
 
