fde9e4cafcee2473cc09b2b7d26a442b7842ceec
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 21:42:24 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:42:24 up 14:27, 12 users,  load average: 0.03, 0.13, 0.06 187e1729f067aaf4891751727151b7a505b198d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -53,5 +53,5 @@ void raise_sw_intr(uint8_t intr_no) {
 	// return address is the next instruction
 	cpu.eip += 2;
 	raise_intr(intr_no);
-	printf("cpu.eip: %x\n", cpu.eip);
+	//printf("cpu.eip: %x\n", cpu.eip);
 }
