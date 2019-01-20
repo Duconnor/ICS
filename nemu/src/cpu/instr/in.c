c20462050701f15bba41b4b6ccf00e0406ea6a13
tracer-pa2018-fall <tracer@njuics.org>
2019-01-20 11:53:43 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:53:43 up 22 min,  8 users,  load average: 0.00, 0.02, 0.05 def452fc73c06a79d22fdc1fe177de141d44af3

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -2,11 +2,12 @@
 #include "device/port_io.h"
 
 make_instr_func(in_b) {
-	operand_read(&opr_src);
-	opr_src.val &= 0xFFFF;
+	//operand_read(&opr_src);
+	//opr_src.val &= 0xFFFF;
 	//printf("port:%x\n", opr_src.val);
-	opr_dest.val = pio_read(opr_src.val, 1);
-	operand_write(&opr_dest);
+	uint32_t data = pio_read(cpu.edx & 0xFFFF, 1);
+	//operand_write(&opr_dest);
+	cpu.eax = data;
 	print_asm_0("in (%%dx), (%%al)", "", 1);
 	return 1;
 }
