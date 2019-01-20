a21d9262a7b9cb52e57ae949b7b711c4ff82fecc
monitor <tracer@njuics.org>
2019-01-20 16:40:01 +0800

    monitor test

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -43,7 +43,8 @@ void irq_handle(TrapFrame *tf) {
 		assert(irq_id < NR_HARD_INTR);
 		//if(irq_id == 0) panic("You have hit a timer interrupt, remove this panic after you've figured out how the control flow gets here.");
 
-		Log("here!\n");
+		//Log("here!\n");
+		panic("hree\n");
 		struct IRQ_t *f = handles[irq_id];
 
 		while (f != NULL) { /* call handlers one by one */
