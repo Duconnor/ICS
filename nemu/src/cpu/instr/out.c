7ce9517729ea6a7a04aa52f6130e15b1ff6e232b
tracer-pa2018-fall <tracer@njuics.org>
2019-01-20 11:50:22 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:50:22 up 19 min,  7 users,  load average: 0.06, 0.05, 0.07 7a8afe46abf1e668796d73254d1314e71636878

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -2,12 +2,14 @@
 #include "device/port_io.h"
 
 make_instr_func(out_b) {
+	/*
 	operand_read(&opr_src);
 	opr_src.val &= 0xFF;
 	operand_read(&opr_dest);
 	opr_dest.val &= 0xFFFF;
+	*/
 	//printf("val:%x", opr_dest.val);
-	pio_write(opr_dest.val, 1, opr_src.val);
+	pio_write(cpu.dx, 1, cpu.al);
 	print_asm_0("out (%%al), (%%dx)", "", 1);
 	return 1;
 }
