0ee7584d8d49acb1d0771cd3bf26690101dc12c3
monitor <tracer@njuics.org>
2019-01-20 11:39:01 +0800

    monitor test

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -3,7 +3,7 @@
 
 make_instr_func(in_b) {
 	operand_read(&opr_src);
-	opr_src.val &= 0xFFFF;
+	opr_src.val &= 0xFF;
 	opr_dest.val = pio_read(opr_src.val, 1);
 	operand_write(&opr_dest);
 	print_asm_0("in (%%dx), (%%al)", "", 1);
