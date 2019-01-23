b48ce7539371ab3033cf22d67d9b59cf751b63a7
monitor <tracer@njuics.org>
2019-01-23 10:34:01 +0800

    monitor test

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -47,7 +47,7 @@ void video_mapping_write_test() {
 void video_mapping_read_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	BREAK_POINT;
+	//BREAK_POINT;
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		assert(buf[i] == i);
 	}
