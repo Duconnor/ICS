60daeeea0e3bedbe957a2183e3cade5ec9f5d281
tracer-pa2018-fall <tracer@njuics.org>
2019-01-23 10:42:09 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:42:09 up  1:04,  6 users,  load average: 0.12, 0.14, 0.06 7a4f3ff5ca044bb4422c8a68bbfd22cea43ba8d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,6 +32,7 @@ uint32_t paddr_read(paddr_t paddr, size_t len) {
 	ret = hw_mem_read(paddr, len);
 #endif
 #else
+	printf("here!\n");
 	int map_NO = is_mmio(paddr);
 	if (map_NO == -1) {
 #ifdef CACHE_ENABLED
@@ -61,7 +62,6 @@ void paddr_write(paddr_t paddr, size_t len, uint32_t data) {
 #else
 	hw_mem_write(paddr, len, data);
 	} else {
-		printf("here!\n");
 		mmio_write(paddr, len, data, map_NO);
 	}
 #endif
