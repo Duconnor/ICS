73610e21e8073383966078cf075b1a45cb68bcdd
monitor <tracer@njuics.org>
2019-01-31 17:10:01 +0800

    monitor test

--- kernel/src/lib/serial.c
+++ kernel/src/lib/serial.c
@@ -15,7 +15,7 @@ void
 serial_printc(char ch) {
 	while (!serial_idle()); // wait untile serial is idle
 	// print 'ch' via out instruction here
-	out_byte(SERIAL_PORT, ch);
+		out_byte(SERIAL_PORT, ch);
 	//panic("%c", ch);
 	//HIT_BAD_TRAP;
 }
