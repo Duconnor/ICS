bc773e502b7293aaba3aae8579f29953c6d7d528
tracer-pa2018-fall <tracer@njuics.org>
2019-02-02 11:22:20 +0800

    >  nemu 161130015 duyx Linux ics-vm 4.9.0-7-686 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:22:20 up 29 min,  8 users,  load average: 0.24, 0.17, 0.07 d03fd27b88d0cb1a3ead815c47906eee154c84c6

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -8,14 +8,13 @@ make_instr_func(out_b) {
 	operand_read(&opr_dest);
 	opr_dest.val &= 0xFFFF;
 	*/
-	//printf("val:%x", opr_dest.val);
+	printf("val:%x", opr_dest.val);
 	pio_write(cpu.edx & 0xFFFF, 1, cpu.eax & 0xFF);
 	print_asm_0("out (%%al), (%%dx)", "", 1);
 	return 1;
 }
 
 make_instr_func(out_v) {
-	printf("%x", cpu.edx & 0xFFFF);
 	pio_write(cpu.edx & 0xFFFF, data_size / 8, cpu.eax);
 	print_asm_0("out (%%ax), (%%dx)", "", 1);
 	return 1;
